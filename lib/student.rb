580e3c6641200afccb0d9a9568501a0b96148534
Martin Moy <martinmoy1991@gmail.com>
2020-03-20 20:09:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,6 +50,12 @@ class Student
     student.save
     student
   end
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
+  end 
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
