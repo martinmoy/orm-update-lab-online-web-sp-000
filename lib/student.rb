6b81b9341f95b18a374c85fb597166ceb632b51d
Martin Moy <martinmoy1991@gmail.com>
2020-03-20 20:09:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,6 +48,7 @@ class Student
   def self.create(name:, grade:)
     student = Student.new(name, grade)
     student.save
+    student
   end
 
   # Remember, you can access your database connection anywhere in this class
