1692021a7b43be53f1d3b2cc09438b7f6d94ef5f
Martin Moy <martinmoy1991@gmail.com>
2020-03-20 20:11:05 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -51,9 +51,9 @@ class Student
     student
   end
   def self.new_from_db(row)
-    id = row[2]
+    id = row[0]
     name = row[1]
-    grade = row[0]
+    grade = row[2]
     self.new(id, name, grade)
   end
 
